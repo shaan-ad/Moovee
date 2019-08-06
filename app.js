32dabd0a89d6d74fe7896784e8551cb4770c7269
shaan-ad <41873763+shaan-ad@users.noreply.github.com>
2019-08-05 22:13:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ var app = express();
 var request = require("request");
 app.ser("view engine", "ejs");
 
+app.get("/", function(req,res){
+  res.render("search");
+});
+
 app.get("/results", function(req,res){
-  request("http://omdbapi.com/?s=california", function(error,response,body){
+  var query = req.query.search;
+  var url = "http://omdbapi.com/?s=" + query;
+  request(url, function(error,response,body){
     if(!error && response.statusCode == 200){
-      var results = JSON.parse(body);
-      res.send(results["Search"][0]["Title"]);
+      var data = JSON.parse(body);
+      res.render("results", {data: data});
     }
   });
 });
